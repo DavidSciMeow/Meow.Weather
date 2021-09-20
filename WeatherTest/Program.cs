82e6636f2c3e16b681c2883e603fc84eb205c3ec
DavidSciMeow <davidtennant405@outlook.com>
2021-09-20 23:13:38 +0800

    Add project files.

---
+++ WeatherTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WeatherTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "山东 青岛";
+            var ss = Meow.Weather.CN.Interpreter.RectifyWord(s.Split(' ')).GetAwaiter().GetResult();
+            Console.WriteLine(ss);
+        }
+    }
+}
